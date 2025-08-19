4e85ba032ab1f056e27e5c8a6885bc9c8d629933
ryo <>
2025-08-19 09:46:46 +0900

    tailwind導入したりDockerfileの調整したりなど（npm周りの修正）

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,38 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+type User = {
+  id: number;
+  name: string;
+};
+
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/users")
-      .then((res: { data: any; }) => setUsers(res.data))
-      .catch((err: any) => console.error(err));
-  }, []);
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/users");
+        setUsers(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchUsers();
+  });
 
   return (
-    <div>
-      <h1>Users</h1>
+    <>
       <ul>
-        {users.map((user: { id: any; name: any; }) => <li key={user.id}>{user.name}</li>)}
+        {users.length === 0 ? (
+          <li>該当なし</li>
+        ) : (
+          users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))
+        )}
       </ul>
-    </div>
+    </>
   );
 }
 
